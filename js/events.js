accdf2e9f0b89ecfb6b9f70ac6259200676da291
Christina Eddington <eddingtonUX@gmail.com>
2020-12-15 12:21:16 -0800

    Added a function for creating tags
    For now it only can create tags from an array of tags and add spans to the ouput div

--- js/events.js
+++ js/events.js
@@ -1,3 +1,27 @@
+function createTags(arr) {
+	let tagId = 0;
+	let output = document.getElementById("output");
+
+	if (output.innerHTML != "") {
+		output.removeChild(output.firstChild);
+	}
+
+	for (let i = 0; i < arr.length; i++) {
+		var span = document.createElement("span");
+		span.appendChild(document.createTextNode(arr[i] + " "));
+		span.className = "tag";
+		span.setAttribute("id", "tag" + tagId++);
+		span.addEventListener("mouseover", function(e) {
+			e.target.style.backgroundColor = "yellow";
+		})
+		span.addEventListener("mouseout", function(e) {
+			e.target.style.backgroundColor = "";
+		})
+		output.appendChild(span);
+	}
+}
+
+
 function parseCode() {
 	let myCode = document.getElementById("myCode");
 	let output = document.getElementById("output");
@@ -5,6 +29,7 @@ function parseCode() {
 	let linesArray = myCode.value.split("\n");
 	console.log("Lines are: "+linesArray);
 	output.innerHTML = myCode.value;
+	createTags(linesArray);
 }
 
 
